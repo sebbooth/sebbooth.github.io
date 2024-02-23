ebf68343898506317f5739b33dcc809cd2900000
sebbooth <sebbooth@yahoo.ca>
2024-02-22 18:13:37 -0800

    first iteration of projects section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,24 +5,27 @@ import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className="navbar-container">
-      <h1>SB</h1>
-      <ul>
-        <li>
-          <a href="#About">About</a>
-        </li>
-        <li>
-          <a href="#Timeline">Timeline</a>
-        </li>
-        <li>
-          <a href="#Projects">Projects</a>
-        </li>
-        <li>
-          <a href="#Contact">Contact</a>
-        </li>
-      </ul>
-      <ThemeSetter />
-    </div>
+    <>
+      <div className="navbar-container">
+        <h1 className="text-logo">SB</h1>
+        <ul className="navbar-links">
+          <li className="navbar-link">
+            <a href="#About">About</a>
+          </li>
+          <li className="navbar-link">
+            <a href="#Timeline">Timeline</a>
+          </li>
+          <li className="navbar-link">
+            <a href="#Projects">Projects</a>
+          </li>
+          <li className="navbar-link">
+            <a href="#Contact">Contact</a>
+          </li>
+        </ul>
+        <ThemeSetter />
+      </div>
+      <div className="navbar-spacer"></div>
+    </>
   );
 };
 
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import { ProjectCard } from "./components";
 import "./Projects.scss";
 
+import data from "../../data/projects.json";
+
 const Projects = () => {
-  return <div>Projects</div>;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      setProjects(data);
+    };
+
+    fetchProjects();
+  }, []);
+
+  return (
+    <section className="projects-section">
+      {/* ... other layout elements */}
+      <div className="projects-container">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
