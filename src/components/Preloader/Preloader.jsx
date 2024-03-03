692d12ab5811434ca7b373c2cc26edbc0aecf1fc
Seb <sebbooth@yahoo.ca>
2024-03-02 17:25:17 -0800

    themed logo using spline events

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -5,8 +5,6 @@ const Preloader = () => {
     <div className="preloader-container">
       <h1>Loading...</h1>
 
-      <h5>This may or may not just be a timed animation</h5>
-      <h5>Let's hope everything loads in time 🙃</h5>
       <div>
         <div>3...2...1...</div>
       </div>
