250d5ac1c749e379e64463a3d0bd0a64eeb935f8
Seb <sebbooth@yahoo.ca>
2024-03-02 16:47:37 -0800

    new branch since spline has issues

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -4,9 +4,6 @@ const Preloader = () => {
   return (
     <div className="preloader-container">
       <h1>Loading...</h1>
-
-      <h5>This may or may not just be a timed animation</h5>
-      <h5>Let's hope everything loads in time 🙃</h5>
       <div>
         <div>3...2...1...</div>
       </div>
